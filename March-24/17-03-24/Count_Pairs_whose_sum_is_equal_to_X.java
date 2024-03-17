83642f68407fbd919367b2a9b07e69bd4fa5d046
Atharvapaymode <96392211+Atharvapaymode@users.noreply.github.com>
2024-03-17 23:16:53 +0530

    Create Count_Pairs_whose_sum_is_equal_to_X.java

---
+++ March-24/17-03-24/Count_Pairs_whose_sum_is_equal_to_X.java
@@ -0,0 +1,16 @@
+https://www.geeksforgeeks.org/problems/count-pairs-whose-sum-is-equal-to-x/1
+class Solution {
+
+    public static int countPairs(LinkedList<Integer> head1, LinkedList<Integer> head2,
+                          int x)
+    {
+         int count=0;
+        HashSet<Integer> temp = new HashSet<>(head1);
+      for(Integer nums : head2){
+          if(temp.contains(x-nums)){
+              count++;
+          }
+      }
+        return count;
+    }
+}
