7d1289b39ca342a2a41f15b6d9a3280ce77b069e
Atharvapaymode <96392211+Atharvapaymode@users.noreply.github.com>
2024-05-20 23:57:41 +0530

    Create Modular_Exponentiation_for_large_numbers.java

---
+++ May-24/20-05-24/Modular_Exponentiation_for_large_numbers.java
@@ -0,0 +1,18 @@
+class Solution
+{
+    public long PowMod(long x, long n, long m)
+    {//exponentiation by squaring method -
+        if(n == 0) return (1 % m);
+        if(n == 2) return (x * x)% m;
+        long ans = 1l;//stores the ans
+        long sq = x;//helps to update the ans
+        while(n > 0){
+            if(n % 2 == 1){//if our power is odd, we update the ans by multiplying with sq
+                ans = (ans * sq)% m;
+            }
+            sq = (sq * sq) % m;//we keep on updating the sq
+            n = n / 2;//and dividing by 2
+        }
+        return ans % m;
+    }
+}
