c48d4eaaad48ef3965694a8a8cc4c38fbceb774e
Atharvapaymode <96392211+Atharvapaymode@users.noreply.github.com>
2024-08-18 23:12:57 +0530

    Create Split_an_array_into_two_equal_Sum_subarrays.java

---
+++ August-24/18-07-24/Split_an_array_into_two_equal_Sum_subarrays.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canSplit(int arr[]) {
+        // code here
+        long sum=0L;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+        }
+        if(sum%2!=0) return false;
+        long sum1=0L;
+        for(int i=0;i<arr.length;i++){
+            sum1+=arr[i];
+            if(sum1==sum/2){
+                return true;
+            }
+        }
+        return false;
+    }
+}
